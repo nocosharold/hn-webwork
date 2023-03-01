d81a7f8d7e20781707a1a86cff81a85a38b6f75b
nocosharold <nocosharold@gmail.com>
2023-03-01 15:28:04 +0800

    Added footer, and Scrollspy

---
+++ assets/js/index.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    $("body")
+         // Get all section elements
+        let sections = $('section');
+        
+        // Create an empty object to store the position of each section
+        let sectionPositions = {};
+        
+        // Loop through each section and store its position relative to the document
+        sections.each(function() {
+            sectionPositions[$(this).attr('id')] = $(this).offset().top - 78;
+        });
+        
+        // Add scroll event listener to window
+        $(window).scroll(function() {
+            // Get the current scroll position
+            let scrollPosition = $(window).scrollTop();
+            
+            // Loop through each section and check if it's in view
+            $.each(sectionPositions, function(key, value) {
+            if (scrollPosition >= value) {
+                // Add 'active' class to the corresponding nav item
+                $('a[href="#' + key + '"]').parent().addClass('active').siblings().removeClass('active');
+
+                // Check if the current section is the contact section
+                // Add 'cssanimation typing sequence' class to the element you want to animate
+                (key == "contact") ? $('.lets_code').addClass('cssanimation typing sequence') : $('.lets_code').removeClass('cssanimation typing sequence');
+                
+            }
+            });
+        });
+});
